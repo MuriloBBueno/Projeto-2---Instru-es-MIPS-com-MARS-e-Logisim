7d64596603fd6ee33c2ed2d5fac49cbbdb4666b5
vinie07 <jbkabr0@proton.me>
2026-05-28 23:29:44 -0300

    Update alarme.asm

--- mars/alarme.asm
+++ mars/alarme.asm
@@ -1,12 +1,12 @@
 .data # RAM
 #--------- Strings de Entrada ---------
-	texto_porta: .asciiz " ...A porta esta aberta? 0 = nao 1 = sim "
-	texto_movimento: .asciiz " ...Foi detectado algum movimento? 0 = nao 1 = sim "
-	texto_chave: .asciiz " ...O sistema está ligado? 0 = nao 1 = sim "
+	texto_porta: .asciiz " A porta esta aberta? 0 = nao 1 = sim "
+	texto_movimento: .asciiz "\nFoi detectado algum movimento? 0 = nao 1 = sim "
+	texto_chave: .asciiz "\nO sistema está ligado? 0 = nao 1 = sim "
 
 #--------- Strings de IF ---------
-	texto_Alarme_Acionado: .asciiz " ...Alarme foi acionado"
-	texto_Alarme_Desacionado: .asciiz " ...Alarme NÃO foi acionado"
+	texto_Alarme_Acionado: .asciiz "\nAlarme foi acionado"
+	texto_Alarme_Desacionado: .asciiz "\nAlarme NÃO foi acionado"
 
 .text # INSTRUÇÕES
 #======================================
@@ -21,8 +21,10 @@
 	li $v0, 5 # parametro para que seja LIDO/ler um inteiro 	
 	syscall # faça isso
 	
-#--- Armazenar em $t1 ---
-	move $t1, $v0 # O resultado da PORTA , esta em T1
+# --- Converter qualquer valor != 0 em 1 ---
+	move $t1, $v0
+	sltu $t1, $zero, $t1 # qualquer != 0 vira 1
+	
 	
 #--- DEBUG (remover em produção ou comentar) ---
 	move $a0, $t1 #Move o resultado final para $a0, que é o argumento para a syscall
@@ -40,10 +42,11 @@
 #--- Ler inteiro do movimento ---
 	li $v0, 5 #codigo para ler/lido um inteiro / capturar
 	syscall 
-
-#--- Armazenar em $t2 ---
-	move $t2, $v0 # o resultado do MOVIMENTO , esta em t2
-
+	
+# --- Converter qualquer valor != 0 em 1 ---
+	move $t2, $v0
+	sltu $t2, $zero, $t2 # qualquer != 0 vira 1
+	
 #--- DEBUG (remover em produção ou comentar)
 	move $a0, $t2 #Move o resultado final para $a0, que é o argumento para a syscall
 	li $v0, 1 #define o codigo da syscall para imprimir inteiro
@@ -60,12 +63,13 @@
 #--- Ler inteiro do movimento ---
 	li $v0, 5 #codigo para ler/lido um inteiro/ capturar do teclado
 	syscall
-
-#--- Armazenar em $t3 ---
-	move $t3, $v0 #o resultado do MOVIMENTO, esta em t3
 	
+# --- Converter qualquer valor != 0 em 1 ---
+	move $t3, $v0
+	sltu $t3, $zero, $t3 # qualquer != 0 vira 1
+
 #--- DEBUG () ---
-	move $a0, $t2 #Move o resultado final para $a0, que é o argumento para a syscall
+	move $a0, $t3 #Move o resultado final para $a0, que é o argumento para a syscall
 	li $v0, 1 #define o codigo da syscall para imprimir inteiro
 	syscall
 
@@ -79,9 +83,8 @@
 # $t3 = CHAVE (0/1)
 
 # --- CLAUSULAS ---
-# $s1 = A+B
-# $s2 = *C
-# $s3 = S
+# $s0 = A+B
+# $s1 = S = *C
 
 # --- Clausula 1 (A+B) ---
 	or $s0,$t1,$t2 # $s0 = A+B
@@ -89,32 +92,41 @@
 # --- Clausula 2 (A+B) * C
 	and $s1,$s0,$t3
 
-# --- = ---
-	move $s2 ,$s1
-
 #======================================
 # ENCERRAMENTO
 #======================================
-#	CODIGO EM C, GUIA:	
-#	if (S = 1){
-#		printf("OK")
-#	}else{
-#		printf("NAO")
+# 	em C:
+#
+# 	if (S != 0){
+# 		printf("ACIONADO");
+# 	} else {
+# 		printf("DESACIONADO");
 #	}
+#
 
-beq $s2, $zero, Negativo # SE S = 0 , vai para negativo(desacionado)
-
-# bloco do else{} (executa se nao desviou):
+# se alarme=0 , PULA para label senao pro if
+beq $s1, $zero, Negativo 
+	
+	#IF (Alarme Ligado)
 	li $v0, 4 # codigo para imprimir string
 	la $a0, texto_Alarme_Acionado  # string do IF ACIONADO
 	syscall 
 		j fimIF # Pula o bloco do fimIF PARA NAO EXECUTAR OS DOIS
 	
+	#Else/label (Alarme Desligado)
 	Negativo:
 		li $v0, 4 # codigo para imprimir string
 		la $a0, texto_Alarme_Desacionado  # string do IF ACIONADO
 		syscall
 
+	#Equivalente a chave de fechamento em C } -> acabou o if e else
 	fimIF:	# --- Return 0 (saída do programa) ---
 		li $v0, 10
 		syscall
+#======================================
+# EXTRA
+#======================================
+# sltu = Set if less than , Unsigned
+# Sintaxe:
+#  sltu $rd, $rs, $rt
+# -> se rS for menor que rT , colocar 1 em rD
